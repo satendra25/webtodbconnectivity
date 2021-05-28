9813ea1ac872b2a2f186c497d9b3420305550afc
satendra singh <satendra25@gmail.com>
2021-05-28 22:47:36 +0530

    initial commit-sampledbconntivity

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using sampledbconnectivity.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sampledbconnectivity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            //ViewBag.Message = "Your application description page.";
+            //return View();
+            List<Emplyee> employeeList = new List<Emplyee>();
+            string CS = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Employee", con);
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    var employee = new Emplyee();
+
+                    employee.EmployeeId = Convert.ToInt32(rdr["EmpId"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employeeList.Add(employee);
+                }
+            }
+            return View(employeeList);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sampledbconnectivity.Models
+{
+    public class Emplyee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+    }
+}
